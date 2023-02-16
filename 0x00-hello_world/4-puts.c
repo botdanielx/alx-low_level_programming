1141eb337937d254ff32ebe3d872067591a54c3c
Adewale <d4dewale@gmail.com>
2023-02-16 19:06:35 +0100

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is the entry point for the program
+ * Return:zero if no error
+ * This program prints our string using  the puts function
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
