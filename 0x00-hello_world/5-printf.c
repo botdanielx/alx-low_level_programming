ab76391ef483751761c326570422dc9681004205
Adewale <d4dewale@gmail.com>
2023-02-16 19:22:33 +0100

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the entry point to the program
+ * Return:zero if no error, non-zero value if errors
+ * Description: This program prints some string using the printf function
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+
+}
